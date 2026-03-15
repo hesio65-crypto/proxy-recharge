558c0ebcecdf10e0fa72905e5f365e1b94abf22d
hesio65-crypto <hesio65@gmail.com>
2026-03-15 17:59:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ app.post("/criar-pix", async (req, res) => {
 
 })
 
-app.listen(3000, () => {
-  console.log("API rodando na porta 3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("API rodando")
 })
