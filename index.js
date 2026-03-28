54cc62fa86229132ea7cb43c58bc124bf8363e06
hesio65-crypto <hesio65@gmail.com>
2026-03-28 18:50:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json())
 // EFI
 const options = {
   sandbox:false,
-  client_id:"Client_Id_c14fdae342370b1c9f86efa3284aa3bc2f592d19",
-  client_secret:"Client_Secret_aeb560538e5e6a1b177ca40be82db0786706f53d",
-  certificate:"./producao-854589-rechargehz3.p12"
+  client_id:"Client_Id_fcfd084f91d7629ee4a0dd4f4330007572c259f7",
+  client_secret:"Client_Secret_eab1cd11838ecd28eee1a454e53a94b5a9afb157",
+  certificate:"./producao-854589-recargahz.p12"
 }
 
 const gn = new Gerencianet(options)
